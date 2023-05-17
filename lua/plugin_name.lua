4c14071f7fe39ca5ec9e56eb40932d9e06cae98a
Murderlon <merlijn@soverin.net>
2023-05-17 19:27:50 +0200

    wip

--- lua/plugin_name.lua
+++ lua/plugin_name.lua
@@ -1,5 +1,7 @@
--- main module file
-local module = require("plugin_name.module")
+local context_manager = require("plenary.context_manager")
+
+local with = context_manager.with
+local open = context_manager.open
 
 local M = {}
 M.config = {
@@ -14,9 +16,18 @@ M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
--- "hello" is a public method for the plugin
-M.hello = function()
-  module.my_first_function()
+M.load = function()
+  local result = with(open("tests/fixtures.json"), function(reader)
+    while true do
+      local line = reader:read()
+      if line == nil then
+        break
+      end
+
+      print(line)
+    end
+  end)
+  return result
 end
 
 return M
--- tests/plugin_name/plugin_name_spec.lua
+++ tests/plugin_name/plugin_name_spec.lua
@@ -1,12 +1,7 @@
 local plugin = require("plugin_name")
 
 describe("setup", function()
-  it("works with default", function()
-    assert("my first function with param = Hello!", plugin.hello())
-  end)
-
-  it("works with custom var", function()
-    plugin.setup({ opt = "custom" })
-    assert("my first function with param = custom", plugin.hello())
+  it("loads", function()
+    print(plugin.load())
   end)
 end)
